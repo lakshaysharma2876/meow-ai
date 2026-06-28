8ba6439e1bb4c7129a5928af5c8b2d03529b2a02
Lakshay Sharma <lakshay2876@gmail.com>
2026-06-28 23:35:38 +0530

    versions resolved

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,11 @@ import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 
 const database = new Pool({
-  connectionString: "postgresql://postgres:password@localhost:5432/database",
+  connectionString:"postgresql://postgres:ZxzFoWpnxYaVtrxC@db.uvyeymmqrxztjbyzevwt.supabase.co:5432/postgres",
 });
 
 export const auth = betterAuth({
   database: database,
+  //set for vercel
   baseURL: "http://localhost:3000/",
 });

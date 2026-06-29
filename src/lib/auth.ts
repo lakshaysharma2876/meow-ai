a5730f651e73774f7bc1dd7a514c8850aee34dd6
Lakshay Sharma <lakshay2876@gmail.com>
2026-06-29 17:34:50 +0530

    Update settings for Google and github oAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,22 @@ import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 
 const database = new Pool({
-  connectionString:"postgresql://postgres:ZxzFoWpnxYaVtrxC@db.uvyeymmqrxztjbyzevwt.supabase.co:5432/postgres",
+  connectionString: process.env.DATABASE_URL
 });
 
 export const auth = betterAuth({
   database: database,
   //set for vercel
   baseURL: "https://meow-ai-lemon.vercel.app/",
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }
+  },
 });
